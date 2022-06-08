84e44095ecd5bca33c12313c31ce9e4e4b130f93
Alexandros Samartzis <77395762+rosi-nantis@users.noreply.github.com>
2022-06-08 11:51:51 +0200

    del files unnececary

--- mushroom_app/NLP_wiki_abstraction.py
+++ mushroom_app/NLP_wiki_abstraction.py
@@ -1,5 +1,6 @@
 # Importing dependencies from transformers
 from transformers import PegasusForConditionalGeneration, AutoTokenizer
+import re
 
 
 
@@ -9,9 +10,24 @@ tokenizer = AutoTokenizer.from_pretrained("google/pegasus-xsum")
 model_pegasus = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
 
 
+mentions_regex= '@[A-Za-z0-9]+'
+url_regex='https?:\/\/\S+' #this will not catch all possible URLs     ###add this to etl script
+hashtag_regex= '#'
+rt_regex= 'RT\s'
+apostroph = "(?i)\\b(?<!')(?![AOI])\\\b"
+
+def clean_text(text):
+    text = re.sub(mentions_regex, '', text)  #removes @mentions
+    text = re.sub(hashtag_regex, '', text) #removes hashtag symbol
+    text = re.sub(rt_regex, '', text) #removes RT to announce retweet
+    text = re.sub(url_regex, '', text) #removes most URLs
+    text = re.sub(apostroph, '', text) #removes most URLs
+    text = re.sub(r"'", " ", text)
+    return text
 
 
-def give_abstract_text(text ):
+def give_abstract_text(text):
+    text = clean_text(text)
     # Create tokens - number representation of our text
     tokens = tokenizer(text, truncation=True, padding="longest", return_tensors="pt")
     # Summarize 
@@ -19,5 +35,6 @@ def give_abstract_text(text ):
     # Decode summary
     text_me = tokenizer.decode(summary[0])
     return text_me
+
 
 
--- mushroom_app/wikipedia_info.py
+++ mushroom_app/wikipedia_info.py
@@ -1,6 +1,6 @@
 #import wikipediaapi
 import wikipedia
-
+import re
 # wiki = wikipediaapi.Wikipedia('en',extract_format=wikipediaapi.ExtractFormat.WIKI)
 # def fetch_wiki_text(prediction):
 #     """This function gives the abstract text of the wiki page search of item
@@ -16,6 +16,22 @@ import wikipedia
     
 #     return url_item.fullurl
 
+mentions_regex= '@[A-Za-z0-9]+'
+url_regex='https?:\/\/\S+' #this will not catch all possible URLs     ###add this to etl script
+hashtag_regex= '#'
+rt_regex= 'RT\s'
+apostroph = "(?i)\\b(?<!')(?![AOI])\\\b"
+
+def clean_text(text):
+    text = re.sub(mentions_regex, '', text)  #removes @mentions
+    text = re.sub(hashtag_regex, '', text) #removes hashtag symbol
+    text = re.sub(rt_regex, '', text) #removes RT to announce retweet
+    text = re.sub(url_regex, '', text) #removes most URLs
+    text = re.sub(apostroph, '', text) #removes most URLs
+    text = re.sub(r"'", " ", text)
+    return text
+
+
 def fetch_wiki_image(prediction):
     """This function gives the first image of the wiki page search of item
     """
@@ -28,6 +44,7 @@ def fetch_wiki_image(prediction):
 
 #fetch_wiki_image('Trametes_versicolor')
 def fetch_wiki_text_alternative(prediction):
+    prediction = clean_text(prediction)
     """This function gives the abstract text of the wiki page search of item
 
     we use this function as alternative of the function fetch_wiki_text() in case the wikepediaapi crashes
